eef747a6656773d5e6ac0bbd96acd2659c545d7a
gnos28 <gnos28@gmail.com>
2023-02-17 10:02:34 +0100

    early boilerplate

---
+++ .github/workflows/deployQA.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# prune docker
+docker stop $(docker ps --filter status=running --filter name=skillhunter -q)
+docker rm -f $(docker ps --filter status=exited -q)
+docker rmi -f $(docker images skillhunter* -q)
+docker image prune -f
+
+# prepare new deployment folder
+mv skillhunter/ oldSkillhunter/
+git clone git@github.com:gnos28/skillhunter.git
+cd skillhunter/
+git pull -f --rebase origin main
+
+# récupérer les .env uploadés précédemment avec scp et les déplacer ici
+mv ../dotenv/.env.frontend front/.env.local
+mv ../dotenv/.env.backend back/.env
+mv ../dotenv/auth.json back/auth.json
+
+# build docker images
+docker compose -f docker-compose.prod.yml build --no-cache
+
+# start containers
+docker compose -f docker-compose.prod.yml up >~/logs/log.compose.$(date +"%s") 2>&1 &
+disown
+
+# delete old folder
+sudo rm -Rf ~/oldSkillhunter/
